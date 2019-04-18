79c4ed4e314fbdd65bf1d30e215cccabd4357598
Kyi Thar Chan Aye <e0395893@u.nus.edu>
2019-04-18 11:37:30 +0800

    create DAO

---
+++ OOAD_CA_Team1/DAO/DBConnect.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace OOAD_CA_Team1.DAO
+{
+    public class DBConnect
+    {
+        SqlConnection cn;
+
+
+        public DBConnect()
+        {
+            cn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["OOAD_CA_ConnString"].ConnectionString);
+        }
+
+        public int SetData(SqlCommand cmd)
+        {
+            if (cn.State == ConnectionState.Closed) cn.Open();
+            cmd.Connection = cn;
+            int rowsAffected = cmd.ExecuteNonQuery();
+            if (cn.State == ConnectionState.Open) cn.Close();
+            return rowsAffected;
+        }
+
+        public DataTable GetData(SqlCommand cmd)
+        {
+            DataTable t1 = new DataTable();
+            cmd.Connection = cn;
+            if (cn.State == ConnectionState.Closed) cn.Open();
+            t1.Load(cmd.ExecuteReader());
+            if (cn.State == ConnectionState.Open) cn.Close();
+            return t1;
+        }
+    }
+}
